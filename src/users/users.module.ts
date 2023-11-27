39a2a6ee047a9e828e8f64a23c997496f07ed1db
Julio Macedo <juliocesar1076@gmail.com>
2023-11-27 11:55:58 -0300

    feat: add users module

---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  providers: [UsersService],
+  controllers: [UsersController],
+})
+export class UsersModule {}
