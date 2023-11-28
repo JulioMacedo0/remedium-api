643cbfb5491b35dd67842daddc4b2893d3c82ac6
Julio Macedo <juliocesar1076@gmail.com>
2023-11-28 17:53:13 -0300

    feat: avoid return passwod

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,8 +9,13 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.prisma.user.create({ data: createUserDto });
+  async create(createUserDto: CreateUserDto) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...User } = await this.prisma.user.create({
+      data: createUserDto,
+    });
+
+    return User;
   }
 
   findAll() {
