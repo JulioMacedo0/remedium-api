4cf546be59791467aea6f28a2bd572f69fa733a3
Julio Macedo <juliocesar1076@gmail.com>
2023-11-29 16:51:09 -0300

    feat: add dto types

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(
+  OmitType(CreateUserDto, ['password']),
+) {
+  @ApiProperty()
+  email?: string;
+  @ApiProperty()
+  username?: string;
+}
