7aa6c8572deea984faa0a3d0aa7f7fd994f8b368
Julio Macedo <juliocesar1076@gmail.com>
2023-11-29 12:00:48 -0300

    feat: change not exist user error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
     const user = await this.prisma.user.findUnique({ where: { email: email } });
 
     if (!user) {
-      throw new NotFoundException(`No user found for email: ${email}`);
+      throw new NotFoundException(`Invalid Credentials`);
     }
 
     if (!comparePassword(password, user.password)) {
