c906be7018ed7a385140f801f5e557376bdc7816
Julio Macedo <juliocesar1076@gmail.com>
2023-12-12 10:54:18 -0300

    feat: update database schema

--- prisma/migrations/20231212135358_db_init/migration.sql
+++ prisma/migrations/20231212135358_db_init/migration.sql
@@ -1,5 +1,8 @@
 -- CreateEnum
-CREATE TYPE "unitOfMeasurament" AS ENUM ('g', 'mg', 'mcg', 'l', 'ml');
+CREATE TYPE "unitOfMeasurament" AS ENUM ('g', 'mg', 'mcg', 'l', 'ml', 'drops', 'units');
+
+-- CreateEnum
+CREATE TYPE "DayOfWeek" AS ENUM ('SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY');
 
 -- CreateTable
 CREATE TABLE "User" (
@@ -9,6 +12,7 @@ CREATE TABLE "User" (
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "expo_token" TEXT[],
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -30,20 +34,40 @@ CREATE TABLE "Alert" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "subtitle" TEXT NOT NULL,
-    "unit_of_measurement" "unitOfMeasurament" NOT NULL,
     "body" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
+    "unit_of_measurement" "unitOfMeasurament" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Alert_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Trigger" (
+    "id" TEXT NOT NULL,
+    "alertId" TEXT NOT NULL,
+    "date" TIMESTAMP(3),
+    "hours" INTEGER,
+    "minutes" INTEGER,
+    "seconds" INTEGER,
+    "week" "DayOfWeek"[],
+    "repeats" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Trigger_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Trigger_alertId_key" ON "Trigger"("alertId");
 
 -- AddForeignKey
 ALTER TABLE "Medicine" ADD CONSTRAINT "Medicine_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Alert" ADD CONSTRAINT "Alert_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Trigger" ADD CONSTRAINT "Trigger_alertId_fkey" FOREIGN KEY ("alertId") REFERENCES "Alert"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
