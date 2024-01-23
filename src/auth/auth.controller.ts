2cdcbbd95e11d10251b0ce8341c27a3b4984d09f
Julio Macedo <juliocesar1076@gmail.com>
2024-01-23 16:27:19 -0300

    feat: add pipe in route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,22 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthEntity } from './entity/auth.entity';
-import { LoginDto } from './dto/login.dto';
+// import { LoginDto } from './dto/login.dto';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
-
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
+import { signInValidationSchema } from 'src/shared/validation-schema/auth';
+import { SignInRequest } from 'src/shared/types/auth/sign-in-dto';
 @Controller('api/v1/auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signIn')
+  @UsePipes(new ZodValidationPipe(signInValidationSchema))
   @ApiOkResponse({ type: AuthEntity })
   login(
-    @Body() { email, password }: LoginDto,
+    @Body() { email, password }: SignInRequest,
     @Res({ passthrough: true }) response: Response,
   ) {
     return this.authService.signIn(response, email, password);
