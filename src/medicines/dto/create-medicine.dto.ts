0b89f6cedc95be55d5897db2126e321fedc7c93b
Julio Macedo <juliocesar1076@gmail.com>
2024-02-02 14:55:05 -0300

    feat: remove class validator

--- src/medicines/dto/create-medicine.dto.ts
+++ src/medicines/dto/create-medicine.dto.ts
@@ -1,14 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
 
 export class CreateMedicineDto {
-  @IsString()
-  @IsNotEmpty({ message: 'Medicine name is required' })
   @ApiProperty()
   name: string;
 
-  @IsNumber()
-  @IsNotEmpty({ message: 'Quantity is required' })
   @ApiProperty()
   quantity: number;
 }
