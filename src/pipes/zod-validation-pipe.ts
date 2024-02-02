5a2af61037dbb81e82f527172c60e0927b9d35bc
Julio Macedo <juliocesar1076@gmail.com>
2024-02-02 14:42:59 -0300

    feat: ignore request dont contain body

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,18 +1,27 @@
-import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import {
+  PipeTransform,
+  Injectable,
+  BadRequestException,
+  ArgumentMetadata,
+} from '@nestjs/common';
 import { ZodSchema, ZodError } from 'zod';
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
 
-  transform(value: unknown) {
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    if (metadata.type != 'body') return value;
     try {
       const parsedValue = this.schema.parse(value);
 
       return parsedValue;
     } catch (error) {
       if (error instanceof ZodError) {
+        console.log(error.issues);
         const errorMsg = error.issues[0].message;
         throw new BadRequestException(`${errorMsg}`);
+      } else {
+        throw new BadRequestException(`${error}`);
       }
     }
   }
