3f42260e85bd1bad895b1544c495b6ea6d885bd6
Julio Macedo <juliocesar1076@gmail.com>
2024-02-02 15:48:59 -0300

    feat: add fields

--- src/shared/validation-schema/alert.ts
+++ src/shared/validation-schema/alert.ts
@@ -1,8 +1,9 @@
-import { AlertType } from '@prisma/client';
+import { AlertType, unitOfMeasurament } from '@prisma/client';
 import { z } from 'zod';
 
 const IntervalAlertSchema = z.object({
   type: z.literal(AlertType.INTERVAL),
+  hour: z.number({ required_error: 'field hour is required' }),
   minute: z.number({ required_error: 'field minute is required' }),
 });
 
@@ -15,10 +16,15 @@ export const createAlertValidationSchema = z.object({
   title: z.string({
     required_error: 'field title is required',
   }),
-  subtitle: z.string().min(1, { message: 'field subtitle is required' }),
-  body: z.string().min(1, { message: 'field subtitle is required' }),
-  medicine_id: z.string().min(1, { message: 'field subtitle is required' }),
-  trigger: z.union([IntervalAlertSchema, DateAlertSchema], {}),
+  subtitle: z.string({ required_error: 'field subtitle is required' }),
+  body: z.string({ required_error: 'field subtitle is required' }),
+  unit_of_measurement: z.nativeEnum(unitOfMeasurament, {
+    required_error: 'field unit_of_measurement is required',
+  }),
+  medicine_id: z.string({ required_error: 'field medicine_id is required' }),
+  trigger: z.union([IntervalAlertSchema, DateAlertSchema], {
+    required_error: 'field trigger is required',
+  }),
 });
 
 export type createAlertDto = z.infer<typeof createAlertValidationSchema>;
