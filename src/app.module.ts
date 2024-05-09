d0edd3305a19fe3afb8008b337931075eb44144b
Julio Macedo <juliocesar1076@gmail.com>
2024-05-09 14:01:46 -0300

    feat: load env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,19 @@ import { PrismaModule } from './prisma/prisma.module';
 import { MedicinesModule } from './medicines/medicines.module';
 import { AlertsModule } from './alerts/alerts.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ScheduleModule.forRoot(),
     AuthModule,
     UsersModule,
     PrismaModule,
     MedicinesModule,
     AlertsModule,
-    ScheduleModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
